4315e68daed0ba0bd18d4b260032a7e38820de02
Jose Ramón Bogarin <bogarin617@gmail.com>
2019-12-04 09:34:28 -0800

    modificando esquema

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,9 +1,6 @@
 import { resolvers } from "./resolvers";
 import { importSchema } from "graphql-import";
 import { makeExecutableSchema } from "graphql-tools";
-//const typeDefs = importSchema(`${__dirname}/schema.graphql`);
 const typeDefs = importSchema(`${__dirname}/schemas/schema.graphql`);
-console.log(typeDefs);
-console.log(__dirname);
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 module.exports = { schema };
